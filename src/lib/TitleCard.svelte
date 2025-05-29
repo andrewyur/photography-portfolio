1036f25e22da2f5b9cdb8f069293f41b316e761a
Andrew Yurovchak <andy@yurovchak.net>
2025-05-29 10:15:08 -0400

    compress hero image

--- src/lib/TitleCard.svelte
+++ src/lib/TitleCard.svelte
@@ -7,7 +7,7 @@
 
 <div>
     <!-- svelte-ignore a11y_missing_attribute -->
-    <img src="/IMG_2507.JPG" style:width="{mobile ? 80 : 60}%">
+    <img src="/IMG_2507.webp" style:width="{mobile ? 80 : 60}%">
     <h1>ANDREW PHOTOGRAPHY</h1>
     <p>I shoot mostly with a Canon Rebel T4i, and ocasionally with my digicam or phone camera. I am not really sure how to classify the type of photos I take, but nearly all of the photos on here are ones I take on walks around my current place of residence. These are all from my shared album on apple photos, learn more about this website <a href="https://github.com/andrewyur/photography-portfolio">here</a>.</p>
 </div>
