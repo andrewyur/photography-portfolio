c479f9528df6ce8ec0f4becdb33df583b5b0f01c
Andrew Yurovchak <andy@yurovchak.net>
2025-07-20 11:12:59 -0400

    loader for title photo

--- src/App.svelte
+++ src/App.svelte
@@ -1,94 +1,102 @@
 <script>
-  import PhotoBatch from "./lib/PhotoBatch.svelte";
-  import { metadata, photoCallbacks } from "./lib/appStores.svelte";
-  import FullscreenViewer from "./lib/FullscreenViewer.svelte";
-  import { setContext } from "svelte";
-    import TitleCard from "./lib/TitleCard.svelte";
+  import PhotoBatch from './lib/PhotoBatch.svelte';
+  import { metadata, photoCallbacks } from './lib/appStores.svelte';
+  import FullscreenViewer from './lib/FullscreenViewer.svelte';
+  import { setContext } from 'svelte';
+  import TitleCard from './lib/TitleCard.svelte';
 
-  let batches = $state([])
-  let doneLoading = $derived((batches.at(-1)?.endIndex ?? 0) >= ($metadata.length ?? 0))
+  let batches = $state([]);
+  let doneLoading = $derived(
+    (batches.at(-1)?.endIndex ?? 0) >= ($metadata.length ?? 0),
+  );
   let scrollableRef;
 
-  setContext("scrollableRef", () => scrollableRef)
+  setContext('scrollableRef', () => scrollableRef);
 
   // fetch photo metadata
   // different in dev vs prod mode
 
   async function loadMetadata() {
-    let metadataUrl = import.meta.env.VITE_BUCKET_URL
+    let metadataUrl = import.meta.env.VITE_BUCKET_URL;
 
     try {
-      const response = await fetch(metadataUrl)
-      $metadata = await response.json()
-      $photoCallbacks = Array($metadata.length)
+      const response = await fetch(metadataUrl);
+      $metadata = await response.json();
+      $photoCallbacks = Array($metadata.length);
     } catch (error) {
-      console.error(error.message)
+      console.error(error.message);
     }
   }
 
   // divide into batches, send each batch to a PhotoBatch component
 
   function addPhotoBatch() {
-    let batchStart = batches.at(-1)?.batchEnd ?? 0 // inclusive
-    let batchEnd = batchStart // exclusive
+    let batchStart = batches.at(-1)?.batchEnd ?? 0; // inclusive
+    let batchEnd = batchStart; // exclusive
 
-    while(batchEnd < $metadata.length && $metadata[batchStart].batchGuid == $metadata[batchEnd].batchGuid) {
-      batchEnd += 1
+    while (
+      batchEnd < $metadata.length &&
+      $metadata[batchStart].batchGuid == $metadata[batchEnd].batchGuid
+    ) {
+      batchEnd += 1;
     }
 
-    const id = import.meta.env.DEV ? batchStart : $metadata[batchStart].batchGuid
+    const id = import.meta.env.DEV
+      ? batchStart
+      : $metadata[batchStart].batchGuid;
 
     const batchProps = {
       id,
       batchStart,
       batchEnd,
-    }
+    };
 
-    batches.push(batchProps)
+    batches.push(batchProps);
   }
 
   // intersection observer, determines when another batch should be added
 
   function loadOnScroll(node) {
-    const intersectDiv = document.getElementById('intersect-div')
-    
-    const intersectionObserver = new IntersectionObserver(([entry]) => {
-      if(entry.isIntersecting && !doneLoading) {
-        addPhotoBatch()
-      }
-    }, {root: intersectDiv, rootMargin: "0px 0px 1000px 0px"})
+    const intersectDiv = document.getElementById('intersect-div');
 
-    intersectionObserver.observe(node)
-  } 
+    const intersectionObserver = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && !doneLoading) {
+          addPhotoBatch();
+        }
+      },
+      { root: intersectDiv, rootMargin: '0px 0px 1000px 0px' },
+    );
+
+    intersectionObserver.observe(node);
+  }
 
   // on first load, need to load enough content to push the sentinel div outside of the root
   // useanimationframe to wait for changes to flush
-  
+
   function initialLoad() {
     requestAnimationFrame(() => {
-      const sentinel = document.getElementById('sentinel')
-      addPhotoBatch()
-      const rect = sentinel.getBoundingClientRect()
-      if(rect.top < window.innerHeight * 2 && !doneLoading)
-        initialLoad()
-    })
+      const sentinel = document.getElementById('sentinel');
+      addPhotoBatch();
+      const rect = sentinel.getBoundingClientRect();
+      if (rect.top < window.innerHeight * 2 && !doneLoading) initialLoad();
+    });
   }
 
-  loadMetadata().then(initialLoad)
+  loadMetadata().then(initialLoad);
 </script>
 
 <main id="intersect-div" bind:this={scrollableRef}>
-  <TitleCard/>
+  <TitleCard />
   {#each batches as batch (batch.id)}
     <PhotoBatch {...batch} />
   {/each}
   <div id="sentinel" use:loadOnScroll></div>
 </main>
 
-<FullscreenViewer/>
+<FullscreenViewer />
 
 <style>
-
   #intersect-div {
     --bg1: #feffbc;
     --bg2: #fff786;
--- src/lib/TitleCard.svelte
+++ src/lib/TitleCard.svelte
@@ -1,23 +1,43 @@
 <script>
     let mobile = $state(window.innerHeight > window.innerWidth);
-    window.addEventListener("resize", () => {
+    window.addEventListener('resize', () => {
         mobile = window.innerHeight > window.innerWidth;
-    })
+    });
+
+    function onLoad(event) {
+        event.target.classList.add('loaded');
+    }
 </script>
 
-<div>
+<div id="container">
     <!-- svelte-ignore a11y_missing_attribute -->
-    <img src="/IMG_2507.webp" style:width="{mobile ? 80 : 60}%">
+    <div id="title-photo" style:width="{mobile ? 80 : 60}%">
+        <img src="/IMG_2507.webp" onload={onLoad} class:loaded={false} />
+    </div>
     <h1>ANDREW PHOTOGRAPHY</h1>
-    <p>I shoot mostly with a Canon Rebel T4i, and ocasionally with my digicam or phone camera. I am not really sure how to classify the type of photos I take, but nearly all of the photos on here are ones I take on walks around my current place of residence. These are all from my shared album on apple photos, learn more about this website <a href="https://github.com/andrewyur/photography-portfolio">here</a>.</p>
+    <p>
+        I shoot mostly with a Canon Rebel T4i, and ocasionally with my digicam
+        or phone camera. I am not really sure how to classify the type of photos
+        I take, but nearly all of the photos on here are ones I take on walks
+        around my current place of residence. These are all from my shared album
+        on apple photos, learn more about this website <a
+            href="https://github.com/andrewyur/photography-portfolio">here</a
+        >.
+    </p>
 </div>
 
 <style>
     img {
-        width: 60%;
+        width: 100%;
         margin-left: auto;
         margin-right: 0%;
         display: block;
+        opacity: 0;
+        transition: opacity 0.2s ease;
+    }
+
+    .loaded {
+        opacity: 1;
     }
 
     h1 {
@@ -25,19 +45,24 @@
         margin-bottom: 40px;
         padding: 0;
     }
-    
+
     p {
         margin: 0;
         padding: 0;
         max-width: 500px;
     }
 
-    div {
+    #container {
         max-width: 1000px;
         margin: 15vh auto;
         /* display: flex;
         flex-direction: column;
         gap: 50px; */
         /* justify-content: space-around; */
+    }
+
+    #title-photo {
+        background-color: rgba(1, 1, 1, 0.1);
+        aspect-ratio: calc(1288 / 966);
     }
 </style>
